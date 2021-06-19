1a50a2d91d14d3a8c50eb82184e32bead3e48a0e
cbedell <charlesbedell5@gmail.com>
2021-06-19 10:32:53 -0400

    added pickle records and manipulation scripts

---
+++ dump_db_pickle_recs.py
@@ -0,0 +1,12 @@
+import pickle
+import glob
+
+for filename in glob.glob('*.pkl'):
+    recfile = open(filename, 'rb')
+    record = pickle.load(recfile)
+    print(filename, '=>\n', record)
+
+suefile = open('sue.pkl', 'rb')
+print(pickle.load(suefile)['name'])
+
+#TODO: Using Shelves pg.23
